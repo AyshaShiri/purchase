6d529e97d73ef74b564119f2e94dc8dfac40b12e
Aysha Shirin <ayshashirin52@gmail.com>
2024-07-11 22:57:24 -0700

    3

--- purchase.py
+++ purchase.py
@@ -4,14 +4,14 @@ data={"apples":[3,5,1,2],"oranges":[0,2,6,7]}
 purchase=pd.DataFrame(data)
 purchase=pd.DataFrame(data,index=['Jane','Robert','Lily','David' ])
 print(purchase)
-p1=purchase.loc['Jane']
+"""p1=purchase.loc['Jane']
 print(p1)
 p1=pd.read_csv(r"C:\Users\DELL\Downloads\purchases.csv")
 print(p1)
 p1=pd.read_csv(r"C:\Users\DELL\Downloads\purchases.csv",index_col=0)
 print(p1)
 p2=pd.read_json(r"C:\Users\DELL\Downloads\purchases.json")
-print(p2)
+print(p2)"""
 con=sqlite3.connect(r"C:\Users\DELL\Downloads\database.db")
 df=pd.read_sql_query("SELECT * FROM purchases",con)
 df1=df.set_index("index",inplace=True)
